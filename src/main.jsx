c4a3dd4fc16f8137c7e79d7b52817c8462c38364
nattharat-naja <nattharatkl@gmail.com>
2025-08-27 13:50:01 +0700

    🎉 start project
    starting project with Vite, adding font file and arranging folder structure.

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		{/* adding your start up page */}
+		{/* like this  <Your page name /> */}
+		<div
+			style={{
+				backgroundColor: "var(--dark-gray-color)",
+				width: "100%",
+				height: "50vh",
+				color: "var(--secondary-text-color)",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				textTransform: "capitalize",
+			}}
+		>
+			hi just hello
+		</div>
+		<div
+			style={{
+				backgroundColor: "var(--background-color)",
+				width: "100%",
+				height: "50vh",
+				color: "var(--primary-text-color)",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				textTransform: "capitalize",
+			}}
+		>
+			hi just hello
+		</div>
+	</StrictMode>
+);
