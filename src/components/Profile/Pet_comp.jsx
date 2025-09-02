08cfa7a77ec547a9bdbb6e17f7d7d532d88b1a2f
boriphat12 <boriphataranyakanon@gmail.com>
2025-09-02 15:28:04 +0700

    implement my pet page + style

--- src/components/Profile/Pet_comp.jsx
+++ src/components/Profile/Pet_comp.jsx
@@ -1,4 +1,34 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Pet_card = ({pet}) => {
+    return (
+        <div>
+            <NavLink to={`/profile/pet/${pet.id}`}>
+                <div className="bg-[var(--cream-color)] rounded p-4 shadow-lg flex flex-col space-y-4 hover:shadow-xl hover:scale-110 transition-all duration-300 cursor-pointer">
+                    <div className="w-full h-full bg-gray-200 rounded mb-4 flex items-center justify-center overflow-hidden">
+                        <img 
+                            src={pet.img}
+                            alt="pet"
+                            className="w-full h-64 object-cover"
+                        />
+                    </div>
+                    <div className="flex justify-between mb-3">
+                            <span className="px-5 py-1 !text-white text-xs rounded-full bg-[var(--dark-brown-color)] font-semibold">
+                                🔴 pet_status
+                            </span>
+                            <div className="text-center">
+                                <p className="text-xl font-semibold">{pet.name}</p>
+                            </div>
+                    </div>
+                    
+                </div>
+                
+            </NavLink>
+        </div>
+        
+    )
+}
 
 const Pet_comp = () => {
     const [pets, setPets] = useState([
@@ -41,12 +71,35 @@ const Pet_comp = () => {
             food_allergy: "Dairy",
             medical_condition: "Healthy",
             img: "https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
+        },
+        {
+            id: 5,
+            name: "Charlie",
+            type: "Dog",
+            breed: "Bulldog",
+            gender: "Female", 
+            food_allergy: "None",
+            medical_condition: "Healthy",
+            img: "https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
         }
     ]);
     return(
         <div>
-
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-8xl">
+                {pets.map(pet => <Pet_card key={pet.id} pet={pet}/>)}
+                <div 
+                    className="bg-[var(--cream-color)] rounded-lg p-6 shadow-lg hover:shadow-xl transition-all duration-300 cursor-pointer border-2 border-dashed border-gray-300 hover:border-gray-400"
+                >
+                    <div className="w-full h-full rounded-lg mb-4 flex items-center justify-center">
+                        <div className="text-6xl !text-gray-400">+</div>
+                    </div>
+                </div>
+                    
+            </div>
+            
         </div>
+        
+        
     )
 }
 export default Pet_comp;
