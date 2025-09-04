0357608343846f97ec6ca3deb6315a6e3a550b55
Sicknine <156204309+SNaytiP@users.noreply.github.com>
2025-09-04 16:49:13 +0700

    style: update footer background color and enhance text alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,26 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-[var(--light-brown-color)] mt-10">
+        <footer className="bg-[var(--cream-color)] mt-10">
             {/* Top Layer */}
-            <div className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center border-b">
+            <div className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center border-b w-full">
                 {/* Left Section */}
-                <div>
+                <div className="w-1/2">
                     <h2 className="text-lg font-semibold ">Pawradise Inn</h2>
-                    <p className="text-sm mt-1">Your pet's favorite place to stay!</p>
+                    <p className="text-sm mt-1">Pawradise Inn was created to meet the needs of pet owners looking for a safe, clean, and comfortable place for their beloved pets. Whether you’re traveling or unable to care for your pet temporarily, our platform makes it easy to book accommodations in advance, check room availability, choose room types, and add extra services.At Pawradise Inn, we ensure every pet receives professional care and a secure, welcoming stay.</p>
                 </div>
                 {/* Right Section */}
-                <div className="flex space-x-4">
-                    <a href="/about" className="text-sm hover:text-gray-400">About Us</a>
-                    <a href="/contact" className="text-sm hover:text-gray-400">Contact</a>
-                    <a href="/privacy" className="text-sm hover:text-gray-400">Privacy Policy</a>
+                    <div className="w-1/2">
+                        <h2 className="text-lg text-right font-semibold ">Follow us</h2>
+                        <p className="text-sm text-right mt-1 cursor-pointer">Facebook</p>
+                        <p className="text-sm text-right mt-1 cursor-pointer">X</p>
+                        <p className="text-sm text-right mt-1 cursor-pointer">Instagram</p>
+                        <p className="text-sm text-right mt-1 cursor-pointer">Email</p>
+                    </div>
                 </div>
-            </div>
-
-            {/* Bottom Layer */}
             <div className="bg-[var(--dark-brown-color)] py-4 ">
-                <div className="max-w-7xl mx-auto px-4 text-center ">
-                    <p className="text-sm text-gray-50">&copy; 2025 Pawradise Inn. All rights reserved.</p>
-                    <p className="text-white text-sm mt-1">Your pet's favorite place to stay!</p>
+                <div className="max-w-7xl mx-auto px-4 text-center">
+                    <p className="text-sm !text-[var(--cream-color)]">&copy; 2025 Pawradise Inn. All rights reserved.</p>
                 </div>
             </div>
         </footer>
