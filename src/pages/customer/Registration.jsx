3e0c2ac26b22141cd620e0940794d8def0fef2e6
nepitunepq <6630283021@student.chula.ac.th>
2025-09-07 01:21:24 +0700

    clean Registration  file + add login page + link done/cancle button + add colour in index.css

--- src/pages/customer/Registration.jsx
+++ src/pages/customer/Registration.jsx
@@ -1,5 +1,8 @@
-import React,{useState} from "react";
-import registerImg from "../../assets/register.png"; //to be change if wanted
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom"; 
+import registerImg from "../../assets/foot.png"; 
+import { handleChange as createHandleChange, validateForm } from "../../utils/HandleRegisterPage";
+
 const fields = [
   { label: "Firstname", name: "Firstname", type: "text", placeholder: "First Name" },
   { label: "Lastname", name: "Lastname", type: "text", placeholder: "Last Name" },
@@ -8,83 +11,94 @@ const fields = [
   { label: "Password", name: "Password", type: "password", placeholder: "Password" },
   { label: "*Confirm Password", name: "ConfirmPassword", type: "password", placeholder: "Confirm Password" },
   { label: "Phone Number", name: "PhoneNumber", type: "text", placeholder: "0xxxxxxxxx" },
-//   { label: "Address", name: "Address", type: "text", placeholder: "Address" },
 ];
 
 const Registration = () => {
-        const [form, setForm] = useState({
-            Firstname: '',
-            Lastname: '',
-            Username: '',
-            Email: '',
-            Password: '',
-            ConfirmPassword: '',
-            PhoneNumber: '',
-            Address: ''
-        });
-        const handleChange = (e) =>{
-            setForm({...form,[e.target.name]:e.target.value});
-        }
-        const handleSubmit = (e) =>{
-            e.preventDefault();
-            //submit form logic here
-            if (form.Password !== form.ConfirmPassword){
-                alert("Password do not match");
-                return;
-            }
-            console.log(form);  
-            aleart("Registration Successful");
-        }
-  return (
-    <div className="w-screen h-screen">
-        <div className="w-[60%] h-full float-left">
-            <img className="w-full h-full object-cover" src={registerImg} alt="Registration Image"/>
-        </div>
+  const [form, setForm] = useState({
+    Firstname: "",
+    Lastname: "",
+    Username: "",
+    Email: "",
+    Password: "",
+    ConfirmPassword: "",
+    PhoneNumber: "",
+    Address: "",
+  });
 
-        <div className="w-[40%] h-full float-right bg-[var(--cream-color)]">
-            <h1 className="text-center pt-5 text-[40px] text-[var(--brown-color)] font-bold"> Registration </h1>
-            <form className="flex flex-col space-y-2 p-5" onSubmit={handleSubmit}>
-                {fields.map((field) => (
-                    <div key={field.name} className="flex flex-col">
-                        <label className="text-[var(--brown-color)] font-semibold" htmlFor={field.name}>{field.label}</label>
-                        <input 
-                            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[var(--light-brown-color)]"
-                            type={field.type} 
-                            name={field.name} 
-                            placeholder={field.placeholder} 
-                            value={form[field.name]}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                ))}
-                <h2 className="text-[var(--brown-color)] font-semibold" > Adress</h2>
-                <textarea
-                    name="Address"
-                    placeholder="Address"
-                    value={form.Address}
-                    onChange={handleChange}
-                    className="border border-gray-300 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-[var(--light-brown-color)]"
-                    rows="3"
-                ></textarea>
-            </form>
-            <div className="flex float-right gap-4 p-2 ml-auto">
-                <button className="w-40 h-10 text-[var(--dark-brown-color)] bg-[var(--light-brown-color)] rounded shadow px-4 py-2  hover:bg-[var(--brown-color)]">
-                    cancel
-                </button>
-                <button className="w-40 h-10 !text-[var(--done-button)] bg-[var(--dark-brown-color)] rounded shadow px-4 py-2">
-                    done
-                </button>
+  const navigate = useNavigate();
+  const handleChange = createHandleChange(form, setForm);
+  const isFormValid = Object.values(form).every((field) => field.trim() !== "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateForm(form)) return;
+    alert("Registration Successful");
+    navigate("/room");
+  };
+
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="w-[60%] h-full ">
+        <img className="w-full h-full object-cover" src={registerImg} alt="Registration" />
+      </div>
+      <div className="w-[40%] h-full bg-[var(--cream-color)]">
+        <h1 className="text-center pt-5 text-[40px] text-[var(--brown-color)] font-bold">
+          Registration
+        </h1>
+        <form className="flex flex-col space-y-2 p-5" onSubmit={handleSubmit}>
+          {fields.map((field) => (
+            <div key={field.name} className="flex flex-col">
+              <label
+                className="text-[var(--brown-color)] font-semibold"
+                htmlFor={field.name}
+              >
+                {field.label}
+              </label>
+              <input
+                className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[var(--light-brown-color)]"
+                type={field.type}
+                name={field.name}
+                placeholder={field.placeholder}
+                value={form[field.name]}
+                onChange={handleChange}
+                required
+              />
             </div>
-        </div>
+          ))}
+          <h2 className="text-[var(--brown-color)] font-semibold">Address</h2>
+          <textarea
+            name="Address"
+            placeholder="Address"
+            value={form.Address}
+            onChange={handleChange}
+            className="border border-gray-300 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-[var(--light-brown-color)]"
+            rows="3"
+          ></textarea>
+          <div className="flex float-right gap-4 p-2 ml-auto">
+            <NavLink to="/login">
+                <button 
+                type="button" 
+                className="w-40 h-10 text-[var(--dark-brown-color)] bg-[var(--light-brown-color)] rounded shadow px-4 py-2"
+                >
+                cancel
+                </button>
+            </NavLink>
+            <button
+              type="submit"
+              disabled={!isFormValid}
+              className={`w-40 h-10 !text-[var(--beige-cream-color)] rounded shadow px-4 py-2 ${
+                isFormValid
+                  ? "bg-[var(--dark-brown-color)]"
+                  : "!text-[var(--beige-cream-color)] bg-[var(--light-brown-color)] cursor-not-allowed"
+              }`}
+            >
+              done
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
-
-const RegistrationFrom =() =>{
-    return(
-        <input/>
-    );
-}
 
 export default Registration;
