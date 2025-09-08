2ee8a9f1e769186048e7bafb61cb2188bc298abe
GnuBurn <ausakorn1909@gmail.com>
2025-09-08 23:08:51 +0700

    Add logic to room's controller

--- controllers/room.js
+++ controllers/room.js
@@ -1,8 +1,10 @@
 const prisma = require('../prisma/prisma');
+const {findRoomById, addRoomPictures, removeRoomPictures} = require('./logics/room');
 
 const getRooms = async (req, res) => {
     try {
         const rooms = await prisma.room.findMany();
+        if(rooms.length === 0) return res.status(404).json({success: false, msg: "No room in database"});
         res.status(200).json({success: true, data: rooms});
     } catch(err){
         res.status(400).json({success: false, error: err.message});
@@ -11,10 +13,9 @@ const getRooms = async (req, res) => {
 
 const getRoom = async (req, res) => {
     try {
-        const room = await prisma.room.findUnique({
-            where: {id: Number(req.params.id)}
-        });
-        if(!room) return res.status(404).json({success: false, msg: 'Room not found'});
+        const roomId = req.params.id;
+        const room = await findRoomById(roomId);
+        if(!room) return res.status(404).json({success: false, msg: 'Room is not found'});
         res.status(200).json({success: true, data: room});
     } catch(err){
         res.status(400).json({success: false, error: err.message});
@@ -23,13 +24,12 @@ const getRoom = async (req, res) => {
 
 const createRoom = async (req, res) => {
     try {
+        const {capacity, price, petType} = req.body;
         const room = await prisma.room.create({
             data: {
-                name: req.body.name,
-                capacity: req.body.capacity,
-                price: req.body.price,
-                petType: req.body.petType,
-                picture: req.body.picture
+                capacity: capacity,
+                price: price,
+                petType: petType,
             }
         });
         res.status(201).json({success: true, data: room});
@@ -40,26 +40,58 @@ const createRoom = async (req, res) => {
 
 const updateRoom = async (req, res) => {
     try {
+        const roomId = req.params.id;
+        const dataToUpdate = {};
+        if (req.body.price !== undefined) dataToUpdate.price = req.body.price;
+        if (req.body.petType !== undefined) dataToUpdate.petType = req.body.petType;
+
+        if (Object.keys(dataToUpdate).length === 0) return res.status(400).json({ success: false, msg: "No valid fields to update" });
+        
         const room = await prisma.room.update({
-            where: {id: Number(req.params.id)},
-            data: req.body
+            where: {id: Number(roomId)},
+            data: dataToUpdate
         });
-        if(!room) return res.status(404).json({success: false, msg: 'Room not found'});
         res.status(200).json({success: true, data: room});
     } catch(err){
+        if (err.code === 'P2025') return res.status(404).json({success: false, msg: 'Room is not found'});
         res.status(400).json({success: false, error: err.message});
     }
 };
 
 const deleteRoom = async (req, res) => {
     try {
+        const roomId = req.params.id;
         const room = await prisma.room.delete({
-            where: {id: Number(req.params.id)}
+            where: {id: Number(roomId)}
         });
-        if(!room) return res.status(404).json({success: false, msg: 'Room not found or already deleted'});
         res.status(200).json({success: true, data: {}});
     } catch(err){
+        if (err.code === 'P2025') return res.status(404).json({success: false, msg: 'Room is not found or already deleted'});
         res.status(400).json({success: false, error: err.message});
+    }
+};
+
+const addPicturesToRoom = async (req, res) =>{
+    try {
+        const roomId = req.params.id;
+        const pictures = req.body.picture;
+        const room = await addRoomPictures(roomId, pictures);
+        res.status(200).json({success: true, data: room});
+    }catch(err){
+        if (err.code === 'P2025') return res.status(404).json({success: false, msg: 'Room is not found'});
+        res.status(400).json({success: false, error: err.message})
+    }
+};
+
+const deletePicturesFromRoom = async (req, res) =>{
+    const roomId = req.params.id;
+    const pictures = req.body.picture;
+    try {
+        const room = await removeRoomPictures(roomId, pictures);
+        res.status(200).json({success: true, data: room});
+    }catch(err){
+        if (err.code === 'P2025') return res.status(404).json({success: false, msg: 'Room is not found'});
+        res.status(400).json({success: false, error: err.message})
     }
 };
 
@@ -68,5 +100,8 @@ module.exports = {
     getRoom,
     createRoom,
     updateRoom,
-    deleteRoom
+    deleteRoom,
+    addPicturesToRoom,
+    deletePicturesFromRoom
 };
+
