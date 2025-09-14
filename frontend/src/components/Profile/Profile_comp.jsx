6783ec9034a2db76fd8ce5186acd838e5ba97fec
boriphat12 <boriphataranyakanon@gmail.com>
2025-09-14 21:15:15 +0700

    implement function to handle on update and cancel

--- frontend/src/components/Profile/Profile_comp.jsx
+++ frontend/src/components/Profile/Profile_comp.jsx
@@ -1,11 +1,36 @@
 
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import { updateUserAPI } from "../../hooks/userAPI";
 
 const Profile_comp = () => {
     const {user, setUser} = useOutletContext();
+    const [newUser, setNewUser] = useState([]);
     
-    if (!user) {
-        return <div>Loading user data...</div>;
+    useEffect(() => {
+        if(user){
+            setNewUser(user)
+        }   
+    }, [user])
+    const handleCancel = () => {
+        const cancel = window.confirm('Are you sure?');
+        if(cancel){
+            setNewUser({...user});
+        }
+    }
+    const handleConfirm = async () => {
+        if (!newUser.id) return;
+        const confirmUpdate = window.confirm('Are you sure to update the data?');
+        if (!confirmUpdate) return;
+
+        try {
+            const updatedUser = await updateUserAPI(newUser.id, newUser);
+            setUser(updatedUser);
+            alert("Profile updated successfully!");
+        } catch (err) {
+            console.error(err);
+            alert("Failed to update profile. Please try again.");
+        }
     }
     return (
         <div>
@@ -23,8 +48,8 @@ const Profile_comp = () => {
                             </label>
                             <input
                                 type="text"
-                                value={user.firstname || ""}
-                                onChange={(e) => setUser({...user, firstname: e.target.value})}
+                                value={newUser.firstname || ""}
+                                onChange={(e) => setNewUser({...newUser, firstname: e.target.value})}
                                 className=
                                 'w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                             />
@@ -35,8 +60,8 @@ const Profile_comp = () => {
                             </label>
                             <input
                                 type="text"
-                                value={user.lastname || ""}
-                                onChange={(e) => setUser({...user, lastname: e.target.value})}
+                                value={newUser.lastname || ""}
+                                onChange={(e) => setNewUser({...newUser, lastname: e.target.value})}
                                 className=
                                 'w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                             />
@@ -49,8 +74,8 @@ const Profile_comp = () => {
                         </label>
                         <input
                             type="text"
-                            value={user.user_name || ""}
-                            onChange={(e) => setUser({...user, user_name: e.target.value})}
+                            value={newUser.user_name || ""}
+                            onChange={(e) => setNewUser({...newUser, user_name: e.target.value})}
                             className= 'w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                         />
                    </div>
@@ -61,8 +86,8 @@ const Profile_comp = () => {
                         </label>
                         <input
                             type="tel"
-                            value={user.phone_number || ""}
-                            onChange={(e) => setUser({...user, phone_number: e.target.value})}
+                            value={newUser.phone_number || ""}
+                            onChange={(e) => setNewUser({...newUser, phone_number: e.target.value})}
                             className= 'w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                         />
                    </div>
@@ -73,8 +98,8 @@ const Profile_comp = () => {
                         </label>
                         <input
                             type="email"
-                            value={user.email || ""}
-                            onChange={(e) => setUser({...user, email: e.target.value})}
+                            value={newUser.email || ""}
+                            onChange={(e) => setNewUser({...newUser, email: e.target.value})}
                             className= 'w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                         />
                    </div>
@@ -84,20 +109,20 @@ const Profile_comp = () => {
                             Address
                         </label>
                         <textarea
-                            value={user.address || ""}
-                            onChange={(e) => setUser({...user, address: e.target.value})}
+                            value={newUser.address || ""}
+                            onChange={(e) => setUser({...newUser, address: e.target.value})}
                             className='w-full px-4 py-3 rounded-lg border-2 transition-all duration-300 border-[var(--brown-color)] bg-[var(--cream-color)] focus:border-[var(--dark-brown-color)] focus:outline-none transition-all duration-300'
                         />
                    </div>
                 </div>
                 <div className="flex justify-end items-center mt-8 pt-6 border-t border-[var(--brown-color)]">
                     <div className="flex space-x-8">
-                        <button onClick={() => console.log("Cancel changes")}
+                        <button onClick={() => handleCancel()}
                                 className="px-6 py-2 border-gray-300 rounded-lg hover:bg-gray-50 transition-colors duration-300 cursor-pointer"
                         >
                             Cancel
                         </button>
-                        <button onClick={() => console.log("Save changes")}
+                        <button onClick={() => handleConfirm()}
                                 className="!text-white px-6 py-2 bg-amber-800 text-white rounded hover:bg-amber-700 transition-colors duration-300 cursor-pointer"    
                         >
                             Done
