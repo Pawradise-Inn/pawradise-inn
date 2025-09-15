cfd4c8cd6c04720265e55139b8f39b02e4e1e2e9
YO9plusplus <6630275021@student.chula.ac.th>
2025-09-16 00:18:53 +0700

    change path in bookedServiceAPI

--- frontend/src/hooks/bookedServiceAPI.js
+++ frontend/src/hooks/bookedServiceAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/v1/bookedService/'
 
 export const getTodayService = async () => {
-  const response = await axios.get(`${API_URL}/dashboard`);
+  const response = await axios.get(`${API_URL}dashboard`);
   return response.data; 
 };
 
