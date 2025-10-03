07b962d1c19bd96258179fa74c7534e035009ab7
Piyaphon <teewccg@gmail.com>
2025-10-03 15:11:58 +0700

    Staff can login/logout and minimize code

--- backend/routes/staff.js
+++ backend/routes/staff.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {updateMyProfile, getStaffProfile} = require('../controllers/staff');
+const {login, logout} = require('../controllers/auth');
 const {protect, authorize} = require('../middleware/auth');
 
 router.put('/:id', protect, authorize('STAFF'), updateMyProfile);
+router.post('/login', login);
+router.post('/logout', protect, logout);
 router.route('/:id')
     .get(getStaffProfile);
 
