6b319e2f936b18c201b4f2def35b137d106fac66
nattharat-naja <nattharatkl@gmail.com>
2025-10-14 21:31:52 +0700

    💄 restyle dropDownList

---
+++ frontend/src/components/DropDownList.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useNotification } from "../context/notification/NotificationProvider";
+import { AnimatePresence, motion } from "motion/react";
+import { dropDown } from "../styles/animation";
+
+const DropDownList = ({
+  startText,
+  options,
+  onChange,
+  value,
+  arrowColor = "white",
+  mainStyle = "",
+  inputSyle = "rounded-xl px-4 py-2 text-2xl outline-0 bg-[var(--light-brown-color)] rounded",
+  dropDownStyle = "border-2 border-[var(--brown-color)] bg-[var(--light-brown-color)]  origin-top translate-y-1 top-full left-0",
+  ...motionProps
+}) => {
+  const [dropDownStatus, setDropDownStatus] = useState(false);
+  const [dropDownSelected, setDropDownSelected] = useState(null);
+  const { createNotification } = useNotification();
+
+  const handleDropDown = () => {
+    if (options.length === 0) {
+      createNotification(
+        "fail",
+        "You have no pet",
+        "Please add you pet first before operate this."
+      );
+      return;
+    }
+    setDropDownStatus(!dropDownStatus);
+  };
+
+  return (
+    <motion.div className={`w-full relative ${mainStyle}`} {...motionProps}>
+      <div
+        onClick={handleDropDown}
+        className={`relative inline-block w-full cursor-pointer ${inputSyle}`}
+      >
+        {value || startText}
+        <i
+          style={{ color: arrowColor }}
+          className={`bi bi-caret-down-fill absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center text-2xl cursor-pointer pointer-events-none transition-all duration-200 ${
+            dropDownStatus && "rotate-180"
+          }`}
+        ></i>
+      </div>
+
+      <AnimatePresence>
+        {dropDownStatus && (
+          <motion.div
+            key="dropDownBlock"
+            variants={dropDown}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            className={`flex flex-col absolute w-full z-50 overflow-hidden ${dropDownStyle}`}
+          >
+            {options.map((option, idx) => (
+              <div key={idx}>
+                <div
+                  onClick={() => {
+                    onChange(option.value);
+                    setDropDownSelected(idx);
+                  }}
+                  className="w-full px-4 py-2 bg-[var(--light-brown-color)] text-xl cursor-pointer relative"
+                >
+                  {dropDownSelected === idx ? (
+                    <motion.div
+                      layoutId="active"
+                      className="w-full px-4 py-2 bg-[var(--cream-color)] text-xl cursor-pointer absolute top-0 left-0 pointer-events-none h-full z-20"
+                    />
+                  ) : null}
+                  <span className="z-30 relative">{option.name}</span>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
+
+export default DropDownList;
