ee2c664da93c988766bbdd99cc970ffa1a1f6dae
nattharat-naja <nattharatkl@gmail.com>
2025-10-15 23:19:53 +0700

    🚀 add scrollTop function

---
+++ frontend/src/context/ScrollUpArrowProvider.jsx
@@ -0,0 +1,54 @@
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+} from "motion/react";
+import { createContext, useContext, useState } from "react";
+
+const ScrollUpArrowContext = createContext();
+
+const ScrollUpArrowProvider = ({ children }) => {
+  const [show, setShow] = useState(true);
+  const [isScrollThreshold, setIsScrollThreshold] = useState(false)
+  const { scrollYProgress } = useScroll();
+
+  useMotionValueEvent(scrollYProgress, "change", () => {
+    if (window.scrollY >= 400) {
+      setIsScrollThreshold(true);
+    } else {
+      setIsScrollThreshold(false);
+    }
+  });
+
+  const handleScrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <ScrollUpArrowContext.Provider value={{ setShow }}>
+      {children}
+      <AnimatePresence>
+        {show && isScrollThreshold && (
+          <motion.i
+            initial={{ opacity: 0, y: "-50px" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50px" }}
+            onClick={handleScrollUp}
+            style={{ zIndex: 2000 }}
+            className="bi bi-arrow-up-circle-fill flex justify-center fixed top-10 right-10 items-center !text-[var(--brown-color)] cursor-pointer scale-200"
+          ></motion.i>
+        )}
+      </AnimatePresence>
+    </ScrollUpArrowContext.Provider>
+  );
+};
+
+const useScrollUpArrow = () => {
+  return useContext(ScrollUpArrowContext);
+};
+
+export { useScrollUpArrow, ScrollUpArrowProvider };
--- frontend/src/pages/staff/PetUpdate.jsx
+++ frontend/src/pages/staff/PetUpdate.jsx
@@ -89,14 +89,14 @@ const PetUpdate = () => {
         >
           {/* My Pets */}
           <div>
-            <div className="space-y-4 max-h-[300px] overflow-y-auto pr-2 scrollbar-sleek">
+            <div className="space-y-4 max-h-[300px] pr-2 scrollbar-sleek">
               {<PetCard pet={pet} />}
             </div>
           </div>
 
           {/* Room Booking */}
           <div className="mt-8">
-            <div className="space-y-4 max-h-[350px] overflow-y-auto pr-2">
+            <div className="space-y-4 max-h-[350px] pr-2">
               <StatusUpdate
                 handleSave={handleSave}
                 handleCancel={handleCancel}
@@ -115,7 +115,7 @@ const PetUpdate = () => {
         >
           <div className="bg-[var(--cream-color)] p-10 rounded-lg shadow-md flex-1 flex flex-col">
             <h2 className="text-2xl font-bold mb-6">History</h2>
-            <div className="space-y-6 overflow-y-auto pr-2 scrollbar-sleek">
+            <div className="space-y-6 pr-2 scrollbar-sleek">
               {serviceData.map((service, index) => (
                 <div key={service.id}>
                   <ServiceCard
