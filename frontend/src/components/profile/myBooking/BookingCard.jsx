114e15fc88ce1e942eb6da89027881b4ea56fbb5
boriphat12 <boriphataranyakanon@gmail.com>
2025-10-17 23:32:52 +0700

    fix date format

--- frontend/src/components/profile/myBooking/BookingCard.jsx
+++ frontend/src/components/profile/myBooking/BookingCard.jsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-
+import { getStatusColor } from "../../../components/staff/StatusUtils";
 export const BookingServiceCard = ({ book, onCancelClick, ...motionProps }) => {
   return (
     <motion.div
@@ -39,6 +39,15 @@ export const BookingServiceCard = ({ book, onCancelClick, ...motionProps }) => {
 };
 
 export const BookingRoomCard = ({room, onCancelClick, ...motionProps}) => {
+  const getDateBlock = (dateWithTime) => {
+      const [date, time] = dateWithTime.replace(".000Z", "").split("T");
+      return (
+        <div>
+          <span className={ `py-1 px-3 rounded-xl mr-2 ${getStatusColor("IDLE")}`}>{date}</span>
+          <span className={ `py-1 px-3 rounded-xl mr-2 ${getStatusColor("IDLE")}`}>{time}</span>
+        </div>
+      );
+    };
   return (
     <motion.div
       className="flex items-center bg-[var(--cream-color)] rounded-lg p-4 shadow-lg mb-6"
@@ -60,11 +69,13 @@ export const BookingRoomCard = ({room, onCancelClick, ...motionProps}) => {
             <span className="text-lg">Pet Name: {room.pet.name || ""}</span>
           </p>
           <p>
-            <span className="text-lg">Checked In: {room.checkIn || ""}</span>
+            <span className="text-lg">Check In</span>
           </p>
+          {getDateBlock(room.checkIn) || ""}
           <p>
-            <span className="text-lg">Checked Out: {room.checkOut || ""}</span>
+            <span className="text-lg">Check Out</span>
           </p>
+          {getDateBlock(room.checkOut) || ""}
         </div>
       </div>
       {/* {Action Button} */}
