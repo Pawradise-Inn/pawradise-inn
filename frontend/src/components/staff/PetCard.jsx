fab2ac2b39c9c86f6d19a6a3486cac38b0b4914f
nepitunepq <6630283021@student.chula.ac.th>
2025-10-20 22:10:31 +0700

    add petStatusColor to use easier + change some of code that does not use color in root

--- frontend/src/components/staff/PetCard.jsx
+++ frontend/src/components/staff/PetCard.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "motion/react";
 import { NavLink } from "react-router-dom";
-import { getStatusColor, getStatusText } from "./StatusUtils";
+import { petStatusColor, getStatusText } from "./StatusUtils";
 import { startUpVariants } from "../../styles/animation";
 const PetCard = ({ pet }) => {
   return (
@@ -35,11 +35,11 @@ const PetCard = ({ pet }) => {
                   </h3>
                 </div>
                 <span
-                  className={`px-4 py-2 text-sm font-semibold rounded-full min-w-[120px] text-center inline-block whitespace-nowrap shadow-md border transition-all duration-300 hover:scale-105 ${getStatusColor(
+                  className={`px-4 py-2 text-sm font-semibold rounded-full min-w-[120px] text-center inline-block whitespace-nowrap shadow-md border transition-all duration-300 hover:scale-105 ${petStatusColor(
                     pet.status
                   )}`}
                 >
-                  {getStatusText(pet.status)}
+                  {(pet.status)}
                 </span>
               </div>
             </div>
