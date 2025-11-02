ca15360061f0d68ccb7c1a099048012c458497b0
nattharat-naja <nattharatkl@gmail.com>
2025-11-02 22:38:03 +0700

    🐛 fix entryDate and exitDate < this day bug

--- frontend/src/utils/HandleValidation.js
+++ frontend/src/utils/HandleValidation.js
@@ -1,11 +1,13 @@
 const getDateValidation = (entryDate, exitDate) => {
   let data = { status: true, warningText: "" };
-  console.log(entryDate, entryDate.getDateValidation)
-  if (entryDate.getTime() === exitDate.getTime()) {
+  console.log(entryDate, entryDate.getDateValidation);
+  if (entryDate.getTime() < new Date() || exitDate.getTime() < new Date()) {
+    data.status = false;
+    data.warningText = "Entry date and Exit date must be later than today.";
+  } else if (entryDate.getTime() === exitDate.getTime()) {
     data.status = false;
     data.warningText = "Entry and exit date cannot be the same.";
-  }
-  if (entryDate.getTime() > exitDate.getTime()) {
+  } else if (entryDate.getTime() > exitDate.getTime()) {
     data.status = false;
     data.warningText = "Entry date must be earlier than exit date.";
   }
