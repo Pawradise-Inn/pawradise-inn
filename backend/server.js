796de26413ff50adfd7bfa2c4a592ad33c7bb1ee
ssrose-b <r.buranapin@gmail.com>
2025-11-12 17:17:29 +0700

    fix: update port configuration from 5001 to 5000 across backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ app.use('/api/v1/payments', payment);
 app.use('/api/v1/qr', qr);
 app.use('/api/v1/slip', slipOk)
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let isInterceptorSetup = false;
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "http://localhost:5000",
 });
 
 export const setUpInterceptors = (logout) => {
