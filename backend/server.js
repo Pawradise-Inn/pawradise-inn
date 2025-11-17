cea008ce7ec850627ee9b33ddaaf0f8c1f855ba6
nattharat-naja <nattharatkl@gmail.com>
2025-11-17 22:43:34 +0700

    🚀🐛 change CORS according to vercel url

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,10 @@ app.use(multerMid.single("image"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pawradise-inn.vercel.app/login"],
+    origin: [
+      "http://localhost:3000",
+      "https://pawradise-inn.vercel.app"
+    ],
     credentials: false,
   })
 );
