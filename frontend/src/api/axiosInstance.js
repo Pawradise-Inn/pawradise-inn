197bb83dd0c248181a9b79adcda8fe0bf0a438f4
nattharat-naja <nattharatkl@gmail.com>
2025-11-17 23:38:01 +0700

    🚀deploy on railway

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let isInterceptorSetup = false;
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 export const setUpInterceptors = (logout) => {
